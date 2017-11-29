e5070dba84f127491f28c994d22a2b3b716cbdc7
Oscar Olsson <happybits@gmail.com>
2017-11-29 10:18:22 +0100

    CustomerRegisterTextfile - ett skal - exempel med SquareRoot

---
+++ CustomerRegisterTextfile/CustomerRegisterTextfile/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CustomerRegisterTextfile.Controllers
+{
+    [Route("Demo")]
+    public class DemoController : Controller
+    {
+        [HttpGet, Route("SquareRoot")]
+        public IActionResult SquareRoot(double? number)
+        {
+            if (number == null)
+            {
+                return BadRequest("Mata in ett riktigt tal!");
+            }
+
+            if (number < 0)
+            {
+                return BadRequest("Det går inte att ta roten ur negativa tal");
+            }
+            var result = Math.Sqrt((double)number);
+
+            return Ok(result);
+        }
+    }
+}

277fedfdd2708f82da3b7fa34a2b4df13da8ff83
Oscar Olsson <happybits@gmail.com>
2017-12-04 07:57:14 +0100

    Projektet CustomerRegisterDatabase. Använd detta projekt som grund för kundregisteret. Har databaskoppling. Kör först migreringsskript.

---
+++ CustomerRegisterDatabase/CustomerRegisterDatabase/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using CustomerRegisterDatabase.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CustomerRegisterDatabase.Controllers
+{
+    [Route("api/customers")]
+    public class CustomerController : Controller
+    {
+        private DatabaseContext databaseContext;
+
+        public CustomerController(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        [HttpPost]
+        public IActionResult Add(Customer customer)
+        {
+
+            databaseContext.Add(customer);
+            databaseContext.SaveChanges();
+
+            return Ok(customer.Id);
+        }
+        
+    }
+}
---
+++ CustomerRegisterDatabase/CustomerRegisterDatabase/wwwroot/js/main.js
@@ -0,0 +1,25 @@
+﻿
+$("#addForm button").click(function () {
+
+    $.ajax({
+        url: '/api/Customers/',
+        method: 'POST',
+        data: {
+            "FirstName": $("#addForm [name=FirstName]").val(),
+        }
+
+    })
+        .done(function (result) {
+
+            alert(`Success! Result = ${result}`)
+            console.log("Success!", result)
+
+        })
+
+        .fail(function (xhr, status, error) {
+
+            alert(`Fail!`)
+            console.log("Error", xhr, status, error);
+
+        })
+});
